e2efbc2686f719307c7e41257bc94735eed8aa79
Osvaldo <ocasabella98@gmail.com>
2026-03-03 21:56:23 -0300

    Primer commit calculadora nueva

---
+++ scripts.js
@@ -0,0 +1,40 @@
+function calcular() {
+
+    var metrosCubiertos = parseFloat(document.getElementById("metrosCubiertos").value) || 0;
+    var metrosGaleria = parseFloat(document.getElementById("metrosGaleria").value) || 0;
+    var metrosPergola = parseFloat(document.getElementById("metrosPergola").value) || 0;
+
+    // ===== LINEA BASE =====
+    var obraGris = 
+        (metrosCubiertos * 400000) +
+        (metrosPergola * 120000) +
+        (metrosGaleria * 250000);
+
+    var llaveEnMano = 
+        (metrosCubiertos * 400000) +
+        (metrosPergola * 250000) +
+        (metrosGaleria * 450000);
+
+    // ===== WOODFRAME 2x4 =====
+    var woodframeGris = 
+        (metrosCubiertos * 500000) +
+        (metrosPergola * 120000) +
+        (metrosGaleria * 250000);
+
+    var woodframeLlave = 
+        (metrosCubiertos * 400000) +
+        (metrosPergola * 250000) +
+        (metrosGaleria * 450000);
+
+    // Mostrar resultados
+    document.getElementById("resultadoObraGris").innerText = "$ " + obraGris.toLocaleString('es-AR');
+    document.getElementById("resultadoLlaveEnMano").innerText = "$ " + llaveEnMano.toLocaleString('es-AR');
+    document.getElementById("resultadoWoodframeGris").innerText = "$ " + woodframeGris.toLocaleString('es-AR');
+    document.getElementById("resultadoWoodframeLlave").innerText = "$ " + woodframeLlave.toLocaleString('es-AR');
+}
+
+function toggleTheme() {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    document.documentElement.setAttribute("data-theme", newTheme);
+}
